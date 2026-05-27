39dbbe9f37b75b3c6769f8229457ff87b6391dea
Rick <rick-2007@outlook.com>
2026-05-27 16:01:38 +0200

    Initial commit, made button with a CClayerColor

---
+++ src/main.cpp
@@ -0,0 +1,48 @@
+#include <Geode/Geode.hpp>
+#include <Geode/modify/LevelSearchLayer.hpp>
+
+using namespace geode::prelude;
+
+class $modify(MyLevelSearchLayer, LevelSearchLayer) {
+  public:
+    bool init(int type) {
+      if (!LevelSearchLayer::init(type)) {
+        return false;
+      }
+
+      auto otherFilterMenu = this->getChildByID("other-filter-menu");
+      auto btnSprite = CCSprite::createWithSpriteFrameName("GJ_plainBtn_001.png");
+      btnSprite->setScale(0.8f);
+      auto button = CCMenuItemSpriteExtra::create(
+        btnSprite,
+        this,
+        menu_selector(MyLevelSearchLayer::onButtonClick)
+      );
+
+      button->setID("randomizer-button");
+      otherFilterMenu->addChild(button);
+
+      auto iconSprite = CCSprite::create("btn-icon.png"_spr);
+      iconSprite->setScale(0.45f);
+      iconSprite->setPosition(button->getContentSize() / 2);
+
+      button->addChild(iconSprite);
+      
+      return true;
+    }
+
+    void onButtonClick(CCObject* sender) {
+      (void)sender;
+
+      auto winSize = CCDirector::sharedDirector()->getWinSize();
+
+      auto layer = CCLayerColor::create({0, 0, 0, 175});
+      layer->setContentSize(winSize);
+
+      auto container = CCSprite::create("pathInnerBG_001.png");
+      container->setPosition(winSize / 2);
+      layer->addChild(container);
+
+      this->addChild(layer, 999);
+    }
+};
